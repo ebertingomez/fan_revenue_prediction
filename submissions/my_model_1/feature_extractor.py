a768e469ada94ebceeb40ca02bc0c9f35c5e1b48
Enrique <enrique.gomez@telecom-paristech.fr>
2019-11-20 19:38:41 +0100

    Edition of the python scripts - WIP

--- submissions/my_model_1/feature_extractor.py
+++ submissions/my_model_1/feature_extractor.py
@@ -24,22 +24,31 @@ class FeatureExtractor(object):
             award.groupby(['Name_processed'])['amount'].agg(['count', 'sum'])
 
         def zipcodes(X):
-            zipcode_nums = pd.to_numeric(X['Zipcode'], errors='coerce')
-            return zipcode_nums.values[:, np.newaxis]
+            zipcode_nums = pd.to_numeric(X['Zipcode'], downcast='integer', errors='coerce')
+            val_prefix = zipcode_nums.values[:, np.newaxis]/1000
+            val_suffix = zipcode_nums.values[:, np.newaxis]%1000
+            return np.concatenate((val_prefix,val_suffix),axis=1)
+
         zipcode_transformer = FunctionTransformer(zipcodes, validate=False)
 
         numeric_transformer = Pipeline(steps=[
-            ('impute', SimpleImputer(strategy='median'))])
+            ('impute', SimpleImputer(strategy='mean'))])
 
-        def process_date(X):
+        def process_date_ordinal(X):
             date = pd.to_datetime(X['Fiscal_year_end_date'], format='%Y-%m-%d')
-            return np.c_[date.dt.year, date.dt.month, date.dt.day]
-        date_transformer = FunctionTransformer(process_date, validate=False)
+            ord_date = date.apply(lambda x: x.toordinal()).values[:,np.newaxis]
+            return ord_date - pd.Timestamp('2013-01-01').toordinal()
 
-        def process_APE(X):
-            APE = X['Activity_code (APE)'].str[:2]
-            return pd.to_numeric(APE).values[:, np.newaxis]
-        APE_transformer = FunctionTransformer(process_APE, validate=False)
+        date_ord_transformer = FunctionTransformer(process_date_ordinal, validate=False)
+
+        def process_numeric_APE(X):
+            APE_prefix = X['Activity_code (APE)'].str[:2]
+            APE_root = X['Activity_code (APE)'].str[2]
+            val_prefix = pd.to_numeric(APE_prefix, errors='coerce').values[:, np.newaxis]
+            val_root = pd.to_numeric(APE_root, errors='coerce').values[:, np.newaxis]
+            return np.concatenate((val_prefix,val_root),axis=1)
+
+        APE_n_transformer = FunctionTransformer(process_numeric_APE, validate=False) 
 
         def merge_naive(X):
             X['Name'] = X['Name'].str.lower()
@@ -49,27 +58,26 @@ class FeatureExtractor(object):
             return df[['count', 'sum']]
         merge_transformer = FunctionTransformer(merge_naive, validate=False)
 
-        num_cols = ['Legal_ID', 'Headcount',
-                    'Fiscal_year_duration_in_months', 'Year']
         zipcode_col = ['Zipcode']
         date_cols = ['Fiscal_year_end_date']
+        drop_cols = ['Name', 'Address', 'City','Fiscal_year_duration_in_months','Year']
         APE_col = ['Activity_code (APE)']
+        num_cols = ['Legal_ID', 'Headcount']
         merge_col = ['Name']
-        drop_cols = ['Address', 'City']
 
         preprocessor = ColumnTransformer(
             transformers=[
-                ('zipcode', make_pipeline(zipcode_transformer,
-                 SimpleImputer(strategy='median')), zipcode_col),
+                ('zipcode', make_pipeline(zipcode_transformer, 
+                SimpleImputer(strategy='constant', fill_value=0)), zipcode_col),
                 ('num', numeric_transformer, num_cols),
-                ('date', make_pipeline(date_transformer,
-                 SimpleImputer(strategy='median')), date_cols),
-                ('APE', make_pipeline(APE_transformer,
-                 SimpleImputer(strategy='median')), APE_col),
-                ('merge', make_pipeline(merge_transformer,
-                 SimpleImputer(strategy='median')), merge_col),
+                ('date', make_pipeline(date_ord_transformer, 
+                SimpleImputer(strategy='mean')), date_cols),
+                ('APE_n', make_pipeline(APE_n_transformer, 
+                SimpleImputer(strategy='constant', fill_value=0)), APE_col),
+                ('merge', make_pipeline(merge_transformer, 
+                SimpleImputer(strategy='median')), merge_col),
                 ('drop cols', 'drop', drop_cols),
-                ])
+            ])
 
         self.preprocessor = preprocessor
         self.preprocessor.fit(X_df, y_array)
