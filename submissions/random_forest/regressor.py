fb17a407caa88570bc98aa912fbe69abb7e53128
Enrique <enrique.gomez@telecom-paristech.fr>
2019-11-25 15:50:39 +0100

    Edition of regressor for Random Forest: Parameters                 n_estimators=5,                         max_depth=35,                         max_features=7,                         min_impurity_decrease=0.018,                         min_impurity_split=0.0063,                         min_weight_fraction_leaf=1e-08 Score: 0.41

--- submissions/random_forest/regressor.py
+++ submissions/random_forest/regressor.py
@@ -5,7 +5,13 @@ from sklearn.base import BaseEstimator
 class Regressor(BaseEstimator):
     def __init__(self):
         self.reg = RandomForestRegressor(
-            n_estimators=10, max_depth=50)
+                        n_estimators=5,
+                        max_depth=35,
+                        max_features=7,
+                        min_impurity_decrease=0.018,
+                        min_impurity_split=0.0063,
+                        min_weight_fraction_leaf=1e-08
+                    )
 
     def fit(self, X, y):
         self.reg.fit(X, y)
